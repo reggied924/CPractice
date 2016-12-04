b9aa7bcecf7c55aecb775fda2ae7d9962c872f54
Reginald Daniel <reginalddaniel924@gmail.com>
2016-12-03 20:26:02 -0500

    Temperature excercise

--- test.c
+++ test.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello, world! This is Reggie checking in\n");
+
+	int fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0; 		/* lower limit of the temperature table */
+	upper = 300; 	/* upper limit */
+	step  = 20; 	/* step size */
+
+	fahr = lower;
+
+	printf("Fahrenheit\tCelsius\n");
+
+	while(fahr <= upper){
+
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+
+	}
 
 	return 0;
 }
