6c7cb4b77ff7ede8a47a5be6817a309bded6fa68
Reginald Daniel <reginalddaniel924@gmail.com>
2016-12-10 23:52:12 -0500

    Begin character io lesson

---
+++ character_io.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+	/* Copy input to output; 1st version */
+
+	int c;
+
+	c = getchar();
+	while(c != EOF){
+		putchar(c);
+		c = getchar();
+	} 
+
+
+	return 0; 
+
+}
